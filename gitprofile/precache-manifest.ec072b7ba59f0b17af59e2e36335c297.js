30b784222aa6da40b0f98c2043493f9b4189a77e
SaroarShahan <shahanntk@gmail.com>
2018-10-10 21:38:58 +0600

    git profile added

---
+++ gitprofile/precache-manifest.ec072b7ba59f0b17af59e2e36335c297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitprofile/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8986f40e80ceb31647c1f3c051b9f02d",
+    "url": "/gitprofile/static/media/bg.8986f40e.png"
+  },
+  {
+    "revision": "8d7ad641b39784ea2225",
+    "url": "/gitprofile/static/js/runtime~main.8d7ad641.js"
+  },
+  {
+    "revision": "c8634caf5855fce5efee",
+    "url": "/gitprofile/static/js/main.c8634caf.chunk.js"
+  },
+  {
+    "revision": "9a879072df31a8e5f0c7",
+    "url": "/gitprofile/static/js/1.9a879072.chunk.js"
+  },
+  {
+    "revision": "c8634caf5855fce5efee",
+    "url": "/gitprofile/static/css/main.7b409d2b.chunk.css"
+  },
+  {
+    "revision": "e7eb374a6de3539159558bc5479b6c02",
+    "url": "/gitprofile/index.html"
+  }
+];
