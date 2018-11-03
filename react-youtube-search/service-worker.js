09ec09f6fef9a4fca3dbf06b38e6b9d54202e2c2
SaroarShahan <shahanntk@gmail.com>
2018-11-03 14:40:11 +0600

    added youtube search videos

--- react-youtube-search/service-worker.js
+++ react-youtube-search/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f49f5d32dfceafb76b3a50ad550c14c.js"
+  "/react-youtube-search/precache-manifest.23ab22fa68b71b51276a2f2b1c7e9a9e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-youtube-search/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
