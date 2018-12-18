48f62d7618b9bad8deac41d7503a1ac7e6968fc5
SaroarShahan <shahanntk@gmail.com>
2018-12-19 00:04:52 +0600

    react todos added

---
+++ react-todo/precache-manifest.79a8e83cedbca261260f51293a18a801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "ada54353f30c3da80e33",
+    "url": "/react-todo/static/js/main.ada54353.chunk.js"
+  },
+  {
+    "revision": "aac75bbadb65a070ad02",
+    "url": "/react-todo/static/js/1.aac75bba.chunk.js"
+  },
+  {
+    "revision": "ada54353f30c3da80e33",
+    "url": "/react-todo/static/css/main.d8c9f376.chunk.css"
+  },
+  {
+    "revision": "9db6754c26c9b328047d90f41f229e71",
+    "url": "/react-todo/index.html"
+  }
+];
