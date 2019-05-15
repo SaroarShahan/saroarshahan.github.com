9923ffae56a2e34583aecda28778e0c0e59645e9
SaroarShahan <shahanntk@gmail.com>
2019-05-15 23:39:34 +0600

    readminator uploaded

---
+++ readminator/precache-manifest.bb3375b885096c67bdf3835b1623157f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b43215cfdad0f9702967d693611069",
+    "url": "/readminator/index.html"
+  },
+  {
+    "revision": "0d02576c4c8bec4ef3db",
+    "url": "/readminator/static/css/3.032ccddc.chunk.css"
+  },
+  {
+    "revision": "944d180f42e8cc889f0f",
+    "url": "/readminator/static/css/5.da1cd3ac.chunk.css"
+  },
+  {
+    "revision": "29bec0967969ad0f59c7",
+    "url": "/readminator/static/css/6.4305271f.chunk.css"
+  },
+  {
+    "revision": "57a22edddbd631b687e3",
+    "url": "/readminator/static/css/main.f28c9c92.chunk.css"
+  },
+  {
+    "revision": "e43887ad30e2e0eeecdc",
+    "url": "/readminator/static/js/0.d929857b.chunk.js"
+  },
+  {
+    "revision": "a6a9955f3424059a9c04",
+    "url": "/readminator/static/js/10.e03ca776.chunk.js"
+  },
+  {
+    "revision": "a9655658c7800ff67303",
+    "url": "/readminator/static/js/11.ffd40e3e.chunk.js"
+  },
+  {
+    "revision": "8da535e31f15d9b401aa",
+    "url": "/readminator/static/js/12.0bd152c5.chunk.js"
+  },
+  {
+    "revision": "0d02576c4c8bec4ef3db",
+    "url": "/readminator/static/js/3.87974bcc.chunk.js"
+  },
+  {
+    "revision": "43d2596161a0bff3d578",
+    "url": "/readminator/static/js/4.813d0528.chunk.js"
+  },
+  {
+    "revision": "944d180f42e8cc889f0f",
+    "url": "/readminator/static/js/5.f8f734a0.chunk.js"
+  },
+  {
+    "revision": "29bec0967969ad0f59c7",
+    "url": "/readminator/static/js/6.319b9fa6.chunk.js"
+  },
+  {
+    "revision": "3789bd14ed7a750bf6db",
+    "url": "/readminator/static/js/7.04ba9524.chunk.js"
+  },
+  {
+    "revision": "53d9234954753ef39337",
+    "url": "/readminator/static/js/8.d8f86633.chunk.js"
+  },
+  {
+    "revision": "b18ce0ded5f61d83563a",
+    "url": "/readminator/static/js/9.dfe971d0.chunk.js"
+  },
+  {
+    "revision": "57a22edddbd631b687e3",
+    "url": "/readminator/static/js/main.3ecda532.chunk.js"
+  },
+  {
+    "revision": "1277f13e941e276cd1eb",
+    "url": "/readminator/static/js/runtime~main.6acb4ded.js"
+  }
+]);
