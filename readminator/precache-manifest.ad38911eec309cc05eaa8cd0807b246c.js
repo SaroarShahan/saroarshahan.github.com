fb8e69a401db795a862083847d01b56e0276a375
SaroarShahan <shahanntk@gmail.com>
2019-05-15 23:56:32 +0600

    readminator readded

--- readminator/precache-manifest.ad38911eec309cc05eaa8cd0807b246c.js
+++ readminator/precache-manifest.ad38911eec309cc05eaa8cd0807b246c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "20a1cfc4f2e62df735b4b90773ec2fb8",
+    "revision": "34351b2a1f5de10e7d3b7fa947973ec7",
     "url": "/readminator/index.html"
   },
   {
@@ -28,8 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/readminator/static/js/10.e03ca776.chunk.js"
   },
   {
-    "revision": "34215d2f5e48c187cc4b",
-    "url": "/readminator/static/js/11.6d185bca.chunk.js"
+    "revision": "82406c2211e8abc06751",
+    "url": "/readminator/static/js/11.9f2181dd.chunk.js"
   },
   {
     "revision": "69989651559aae5de901",
@@ -68,7 +68,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/readminator/static/js/main.7ac50382.chunk.js"
   },
   {
-    "revision": "80bafd76e8753b613d44",
-    "url": "/readminator/static/js/runtime~main.5d350f26.js"
+    "revision": "d6eb41ed762d05571c63",
+    "url": "/readminator/static/js/runtime~main.589ef853.js"
   }
 ]);
--- readminator/service-worker.js
+++ readminator/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/readminator/precache-manifest.38e6223cf2a4588e88e0db57fcf242b1.js"
+  "/readminator/precache-manifest.ad38911eec309cc05eaa8cd0807b246c.js"
 );
 
 self.addEventListener('message', (event) => {
