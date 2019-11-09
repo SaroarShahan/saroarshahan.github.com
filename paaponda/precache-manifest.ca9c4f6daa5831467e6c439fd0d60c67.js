24d8fec0da03de00ba4e57c0e4d0debb6d49cf36
SaroarShahan <shahanntk@gmail.com>
2019-11-09 12:33:32 +0600

    fixed breadcrumbs issue

---
+++ paaponda/precache-manifest.ca9c4f6daa5831467e6c439fd0d60c67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705c6cbd104bc831200d6071370a6daa",
+    "url": "/paaponda/index.html"
+  },
+  {
+    "revision": "bd6f0d1919531d94253f",
+    "url": "/paaponda/static/css/3.c829125e.chunk.css"
+  },
+  {
+    "revision": "2d2d9cc90dad76d72b57",
+    "url": "/paaponda/static/js/2.2280d9ae.chunk.js"
+  },
+  {
+    "revision": "bd6f0d1919531d94253f",
+    "url": "/paaponda/static/js/3.78cfc4d1.chunk.js"
+  },
+  {
+    "revision": "fda02947ff21ddf0fd4e",
+    "url": "/paaponda/static/js/4.f8d7a524.chunk.js"
+  },
+  {
+    "revision": "a476396007e9cf443eea",
+    "url": "/paaponda/static/js/5.2c0d7e4d.chunk.js"
+  },
+  {
+    "revision": "82a7aedefc8035024ed6",
+    "url": "/paaponda/static/js/6.e3dd8bcf.chunk.js"
+  },
+  {
+    "revision": "c15e89134ed64e6fc220",
+    "url": "/paaponda/static/js/main.4454f15f.chunk.js"
+  },
+  {
+    "revision": "e540a37adff24ae364be",
+    "url": "/paaponda/static/js/runtime-main.cf1d1340.js"
+  },
+  {
+    "revision": "a4afcfd3c3db3a28490f6844ae45cde8",
+    "url": "/paaponda/static/media/saroarshahan.a4afcfd3.jpg"
+  }
+]);
