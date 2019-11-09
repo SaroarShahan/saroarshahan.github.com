f1fcb259cfa76d651a222ed5dd0160d4a1f10e97
SaroarShahan <shahanntk@gmail.com>
2019-11-09 10:44:08 +0600

    paaponda added

---
+++ paaponda/precache-manifest.4a4afa0befbacfc8e045bbc854c64954.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f12c205043895c1be1f1304279d634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2010a58475ac2fa016ac",
+    "url": "/static/css/3.c829125e.chunk.css"
+  },
+  {
+    "revision": "2d2d9cc90dad76d72b57",
+    "url": "/static/js/2.2280d9ae.chunk.js"
+  },
+  {
+    "revision": "2010a58475ac2fa016ac",
+    "url": "/static/js/3.fda57cd5.chunk.js"
+  },
+  {
+    "revision": "fda02947ff21ddf0fd4e",
+    "url": "/static/js/4.f8d7a524.chunk.js"
+  },
+  {
+    "revision": "4b424a169a83df07d2bc",
+    "url": "/static/js/5.6ad6f5b0.chunk.js"
+  },
+  {
+    "revision": "eba016f959bba04b1673",
+    "url": "/static/js/6.2e54bad3.chunk.js"
+  },
+  {
+    "revision": "e89e82e14fc6f6a80eaa",
+    "url": "/static/js/main.bf7c8d06.chunk.js"
+  },
+  {
+    "revision": "f35c7faa0d820b24cf4f",
+    "url": "/static/js/runtime-main.43c8220b.js"
+  },
+  {
+    "revision": "a4afcfd3c3db3a28490f6844ae45cde8",
+    "url": "/static/media/saroarshahan.a4afcfd3.jpg"
+  }
+]);
